d9073ba36df6257bee822546fe5214b1d43fb694
CaseyCrumb <caseyncrumb@gmail.com>
2023-11-01 21:58:15 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { MovieProvider } from './context/MovieContext';
 import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList';
+import HomePage from './pages/HomePage';
 import './App.css';
+
 
 function App() {
   return (
     <div>
       <MovieProvider>
+        <HomePage />
         <SearchBar />
         <MovieList />
       </MovieProvider>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieList from '../components/MovieList';
+
+const HomePage= () => {
+    return (
+        <div className="home-page">
+            <MovieList />
+        </div>
+    );
+}
+
+export default HomePage;
